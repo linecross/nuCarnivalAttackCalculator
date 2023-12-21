896d0373100bd9ed6df0d1959d4c055989ac8f81
crossline <writercrossline@gmail.com>
2023-12-21 20:39:09 +0800

    Initial commit

--- res/js/uiMain.js
+++ res/js/uiMain.js
@@ -1,6 +1,6 @@
-import { Character, Rarity, Element, AttackType, ActionPattern } from '../../../build/Constants.js';
-import { CardCenter, Team, Battle, Condition } from '../../../build/BattleSystem.js';
-import { CardData } from '../../../build/SampleData.js';
+import { Character, Rarity, Element, AttackType, ActionPattern } from 'Constants';
+import { CardCenter, Team, Battle, Condition } from 'BattleSystem';
+import { CardData } from 'SampleData';
 
 
 var config = {
