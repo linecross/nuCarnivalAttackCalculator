2b4eeac4f5857dbd7a4c000595469705ab002cb0
crossline <writercrossline@gmail.com>
2024-10-22 22:37:31 +0800

    Update test case after 蜜話加成

--- src/__tests__/Battle.real_data.test.ts
+++ src/__tests__/Battle.real_data.test.ts
@@ -69,76 +69,76 @@ describe('實際測試', () => {
             cards = ['SR奧利文', '學院．八雲', '雙月．崑西', '賭場．艾斯特', '賭場．墨菲']
             setupBattle();
 
-            expect(battle.getTurnValue('賭場．艾斯特', 13)).toBeAround(43403);
-            expect(battle.getTurnValue('賭場．墨菲', 13)).toBeAround(58578);
+            expect(battle.getTurnValue('賭場．艾斯特', 13)).toBeAround(45321);
+            expect(battle.getTurnValue('賭場．墨菲', 13)).toBeAround(61179);
 
-            expect(battle.getTurnValue('學院．八雲', 14)).toBeAround(82208);
-            expect(battle.getTurnValue('雙月．崑西', 14)).toBeAround(87250);
+            expect(battle.getTurnValue('學院．八雲', 14)).toBeAround(85240);
+            expect(battle.getTurnValue('雙月．崑西', 14)).toBeAround(90506);
             
-            expect(battle.getTotalValue('學院．八雲')).toBeAround(502806);
-            expect(battle.getTotalValue('雙月．崑西')).toBeAround(467939);
-            expect(battle.getTotalValue('賭場．艾斯特')).toBeAround(319473);
-            expect(battle.getTotalValue('賭場．墨菲')).toBeAround(407492);
+            expect(battle.getTotalValue('學院．八雲')).toBeAround(525177);
+            expect(battle.getTotalValue('雙月．崑西')).toBeAround(487897);
+            expect(battle.getTotalValue('賭場．艾斯特')).toBeAround(334733);
+            expect(battle.getTotalValue('賭場．墨菲')).toBeAround(426602);
         });
 
         test('SR奧+普八+軍布+霜狼+偶奧', () => {
             cards = ['SR奧利文', '常駐．八雲', '軍服．布儡', '晶霜．可爾', '偶像．奧利文']
             setupBattle();
 
-            expect(battle.getTurnValue('常駐．八雲', 13)).toBeAround(43882);
-            expect(battle.getTurnValue('軍服．布儡', 13)).toBeAround(71058);
-            expect(battle.getTurnValue('晶霜．可爾', 13)).toBeAround(82746);
-            expect(battle.getTurnValue('偶像．奧利文', 13)).toBeAround(68970);
+            expect(battle.getTurnValue('常駐．八雲', 13)).toBeAround(45900);
+            expect(battle.getTurnValue('軍服．布儡', 13)).toBeAround(74218);
+            expect(battle.getTurnValue('晶霜．可爾', 13)).toBeAround(86148);
+            expect(battle.getTurnValue('偶像．奧利文', 13)).toBeAround(72054);
             
-            expect(battle.getTotalValue('常駐．八雲')).toBeAround(269600);
-            expect(battle.getTotalValue('軍服．布儡')).toBeAround(277464);
-            expect(battle.getTotalValue('晶霜．可爾')).toBeAround(381960);
-            expect(battle.getTotalValue('偶像．奧利文')).toBeAround(405183);
+            expect(battle.getTotalValue('常駐．八雲')).toBeAround(282752);
+            expect(battle.getTotalValue('軍服．布儡')).toBeAround(290204);
+            expect(battle.getTotalValue('晶霜．可爾')).toBeAround(398615);
+            expect(battle.getTotalValue('偶像．奧利文')).toBeAround(424566);
         });
 
         test('學奧+普八+軍布+霜狼+偶奧', () => {
             cards = ['學院．奧利文', '白情．八雲', '追夏．啖天', '沙漠．啖天', 'SR崑西']
             setupBattle();
 
-            expect(battle.getTurnValue('白情．八雲', 13)).toBeAround(92465);
-            expect(battle.getTurnValue('追夏．啖天', 13)).toBeAround(86892);
-            expect(battle.getTurnValue('沙漠．啖天', 13)).toBeAround(178611);
-            expect(battle.getTurnValue('SR崑西', 13)).toBeAround(327075);
+            expect(battle.getTurnValue('白情．八雲', 13)).toBeAround(98234);
+            expect(battle.getTurnValue('追夏．啖天', 13)).toBeAround(92322);
+            expect(battle.getTurnValue('沙漠．啖天', 13)).toBeAround(189762);
+            expect(battle.getTurnValue('SR崑西', 13)).toBeAround(337611);
             
-            expect(battle.getTotalValue('白情．八雲')).toBeAround(460677);
-            expect(battle.getTotalValue('追夏．啖天')).toBeAround(478576);
-            expect(battle.getTotalValue('沙漠．啖天')).toBeAround(869967);
-            expect(battle.getTotalValue('SR崑西')).toBeAround(783089);
+            expect(battle.getTotalValue('白情．八雲')).toBeAround(489430);
+            expect(battle.getTotalValue('追夏．啖天')).toBeAround(508487);
+            expect(battle.getTotalValue('沙漠．啖天')).toBeAround(924315);
+            expect(battle.getTotalValue('SR崑西')).toBeAround(806565);
         });
 
         test('錆玖+鍍副+血啖+血奧+煙八', () => {
             cards = ['錆色．玖夜', '鍍金．艾德蒙特', '血鑰．啖天', '血鑰．奧利文', '煙嵐．八雲']
             setupBattle();
 
-            expect(battle.getTurnValue('鍍金．艾德蒙特', 13)).toBeAround(81360);
-            expect(battle.getTurnValue('血鑰．啖天', 13)).toBeAround(118638);
-            expect(battle.getTurnValue('血鑰．奧利文', 13)).toBeAround(275010);
-            expect(battle.getTurnValue('煙嵐．八雲', 13)).toBeAround(336259);
+            expect(battle.getTurnValue('鍍金．艾德蒙特', 13)).toBeAround(83423);
+            expect(battle.getTurnValue('血鑰．啖天', 13)).toBeAround(121722);
+            expect(battle.getTurnValue('血鑰．奧利文', 13)).toBeAround(282102);
+            expect(battle.getTurnValue('煙嵐．八雲', 13)).toBeAround(347142);
             
-            expect(battle.getTotalValue('鍍金．艾德蒙特')).toBeAround(452591);
-            expect(battle.getTotalValue('血鑰．啖天')).toBeAround(615852);
-            expect(battle.getTotalValue('血鑰．奧利文')).toBeAround(768827);
-            expect(battle.getTotalValue('煙嵐．八雲')).toBeAround(995491);
+            expect(battle.getTotalValue('鍍金．艾德蒙特')).toBeAround(467377);
+            expect(battle.getTotalValue('血鑰．啖天')).toBeAround(635547);
+            expect(battle.getTotalValue('血鑰．奧利文')).toBeAround(792364);
+            expect(battle.getTotalValue('煙嵐．八雲')).toBeAround(1031766);
         });
 
         test('聖啖+學八+常副+錆歛+SR艾斯特', () => {
             cards = ['聖夜．啖天', '學院．八雲', '常駐．艾德蒙特', '錆色．歛', 'SR艾斯特']
             setupBattle();
 
-            expect(battle.getTurnValue('學院．八雲', 14)).toBeAround(52746);
-            expect(battle.getTurnValue('常駐．艾德蒙特', 14)).toBeAround(38964);
-            expect(battle.getTurnValue('錆色．歛', 14)).toBeAround(43656);
+            expect(battle.getTurnValue('學院．八雲', 14)).toBeAround(56045);
+            expect(battle.getTurnValue('常駐．艾德蒙特', 14)).toBeAround(41399);
+            expect(battle.getTurnValue('錆色．歛', 14)).toBeAround(46386);
             expect(battle.getTurnValue('SR艾斯特', 14)).toBeAround(21765);
             
-            expect(battle.getTotalValue('學院．八雲')).toBeAround(464190);
-            expect(battle.getTotalValue('常駐．艾德蒙特')).toBeAround(402057);
-            expect(battle.getTotalValue('錆色．歛')).toBeAround(516900);
-            expect(battle.getTotalValue('SR艾斯特')).toBeAround(324643);
+            expect(battle.getTotalValue('學院．八雲')).toBeAround(493204);
+            expect(battle.getTotalValue('常駐．艾德蒙特')).toBeAround(427183);
+            expect(battle.getTotalValue('錆色．歛')).toBeAround(549209);
+            expect(battle.getTotalValue('SR艾斯特')).toBeAround(328275);
         });
 
     });
